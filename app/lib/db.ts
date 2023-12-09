75b0fc11efd0e338bd1edf94b1e01718bbc99228
sanjeev <bhusalsanjeev23@gmail.com>
2023-12-09 20:15:12 +0545

    fix: prisma creating multiple clients

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | null;
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
 export { prisma };
