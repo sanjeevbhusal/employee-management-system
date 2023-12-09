d7fa50d9fea76f8a0e1c8add5c7f5c9cbebaeeea
sanjeev <bhusalsanjeev23@gmail.com>
2023-12-09 20:24:47 +0545

    add routes to navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -21,17 +21,49 @@ import {
 import { RxHamburgerMenu } from "react-icons/rx";
 
 import { signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 function NavBar() {
   const user = useSession();
-  console.log(user);
-  if (user) {
-    user.image = "";
-  }
+  const [openHamburgerMenu, setOpenHamburgerMenu] = useState(false);
+  const pathname = usePathname();
+
+  console.log(
+    pathname,
+    pathname.includes("/all-employees"),
+    pathname.includes("/create-employee"),
+  );
+
   return (
     <div className="flex items-center justify-between border-b px-4 py-4 lg:px-20">
       <h1 className="text-lg font-semibold">Employee Management System</h1>
-      <div>
+      <div className="flex items-center gap-4">
+        <Link href="/all-employees" className="hidden lg:block">
+          <Button
+            variant="link"
+            className={
+              pathname.includes("/all-employees")
+                ? "text-sky-600 underline"
+                : ""
+            }
+          >
+            All Employees
+          </Button>
+        </Link>
+        <Link href="/create-employee" className="hidden lg:block">
+          <Button
+            variant="link"
+            className={
+              pathname.includes("/create-employee")
+                ? "text-sky-600 underline"
+                : ""
+            }
+          >
+            Create Employee
+          </Button>
+        </Link>
         {!user ? (
           <Button variant={"link"} onClick={() => signIn("google")}>
             Login
@@ -55,12 +87,31 @@ function NavBar() {
               </DropdownMenu>
             </div>
             <div className="block lg:hidden">
-              <Sheet>
+              <Sheet
+                open={openHamburgerMenu}
+                onOpenChange={() => setOpenHamburgerMenu(!openHamburgerMenu)}
+              >
                 <SheetTrigger>
                   <RxHamburgerMenu size={24} />
                 </SheetTrigger>
                 <SheetContent>
                   <SheetHeader>
+                    <Link href="/all-employees" className="">
+                      <Button
+                        variant="link"
+                        onClick={() => setOpenHamburgerMenu(false)}
+                      >
+                        All Employees
+                      </Button>
+                    </Link>
+                    <Link href="/create-employee" className="">
+                      <Button
+                        variant="link"
+                        onClick={() => setOpenHamburgerMenu(false)}
+                      >
+                        Create Employee
+                      </Button>
+                    </Link>
                     <Button variant={"ghost"} className="justify-start">
                       Your Profile
                     </Button>
